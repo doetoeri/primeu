1609b743b00f7b6ef267592c44dd4dce54d5aeba
doetoeri <doetoeri@gmail.com>
2024-12-20 13:32:06 +0900

    Update generatePrime.js

--- pages/api/generatePrime.js
+++ pages/api/generatePrime.js
@@ -3,7 +3,6 @@ export default function handler(req, res) {
         const start = parseInt(req.query.start || '2', 10);
         let num = start;
 
-        // 소수 찾기 함수
         function isPrime(n) {
             if (n < 2) return false;
             for (let i = 2; i * i <= n; i++) {
